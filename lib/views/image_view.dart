c142b05b4a8c657d32c846bb88b79e32ef0eb437
Nathan Rihet <nathan.rihet06@gmail.com>
2021-06-01 16:52:10 +0200

    sliver work but problem with color modal

--- lib/views/image_view.dart
+++ lib/views/image_view.dart
@@ -19,171 +19,100 @@ class ImageView extends StatefulWidget {
 
 class _ImageViewState extends State<ImageView> {
   var filePath;
+  static const double _appBarBottomBtnPosition = 24.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: CustomScrollView(
-          slivers: <Widget> [
-            SliverAppBar(
-              expandedHeight:  MediaQuery.of(context).size.height,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Image.network(
-                  widget.imgUrl,
-                  fit: BoxFit.cover,
+        body: Container(
+          color: Colors.blue,
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                pinned: true,
+                expandedHeight: MediaQuery.of(context).size.height,
+                flexibleSpace: FlexibleSpaceBar(
+                  titlePadding: EdgeInsets.only(bottom: 25),
+                  title: buttonDownload(context),
+                  background: Image.network(
+                    widget.imgUrl,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-
-            )
-          ],
-
-/*        child: Column(
-        children: <Widget>[
-          Hero(
-            tag: widget.imgUrl,
-            child: Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                child: Image.network(
-                  widget.imgUrl,
-                  fit: BoxFit.cover,
-                )),
           ),
-          GestureDetector(
-            onVerticalDragEnd: (endDetails) {
-              double velocity = endDetails.primaryVelocity;
-              if (velocity < 0) {
-                _detailsModal(context);
-              } else {
-                Navigator.pop(context);
-              };
-            },
+                 SliverPadding(
+                   padding: EdgeInsets.only(top: _appBarBottomBtnPosition),
+                 ),
+                SliverFixedExtentList(
+                  itemExtent: 400.00, delegate: SliverChildListDelegate([
+                detailsModal(context)
+              ],),)
+            ],
           ),
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            alignment: Alignment.topLeft,
-          ),
-          Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              alignment: Alignment.bottomCenter,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {
-                      _save();
-                    },
-                    child: Stack(
-                      children: <Widget>[
-                        Container(
-                          height: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Color(0xff1C1B1B).withOpacity(0.8),
-                          ),
-                          width: MediaQuery.of(context).size.width / 2,
-                        ),
-                        Container(
-                          height: 50,
-                          width: MediaQuery.of(context).size.width / 2,
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                          decoration: BoxDecoration(
-                              border:
-                                  Border.all(color: Colors.white54, width: 1),
-                              borderRadius: BorderRadius.circular(30),
-                              gradient: LinearGradient(colors: [
-                                Color(0x36FFFFFF),
-                                Color(0x0EFFFFFF)
-                              ])),
-                          child: Column(
-                            children: <Widget>[
-                              Text(
-                                "Set Wallpaper",
-                                style: TextStyle(
-                                    fontSize: 16, color: Colors.white70),
-                              ),
-                              Text(
-                                "image will be saved in gallery",
-                                style: TextStyle(
-                                    fontSize: 10, color: Colors.white70),
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      _detailsModal(context);
-                    },
-                    child: Text(
-                      "Details",
-                      style: TextStyle(
-                        color: Colors.white,
-                        shadows: [
-                          Shadow(
-                              // bottomLeft
-                              offset: Offset(-1, -1),
-                              color: Colors.black87),
-                          Shadow(
-                              // bottomRight
-                              offset: Offset(1, -1),
-                              color: Colors.black87),
-                          Shadow(
-                              // topRight
-                              offset: Offset(1, 1),
-                              color: Colors.black87),
-                          Shadow(
-                              // topLeft
-                              offset: Offset(-1, 1),
-                              color: Colors.black87),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 50,
-                  )
-                ],
-              ))
-        ],
         ),
- */     ),
-      ),
     );
   }
 
-  _save() async {
-    if (Platform.isAndroid) {
-      await _askPermission();
-    }
-    var response = await Dio()
-        .get(widget.imgUrl, options: Options(responseType: ResponseType.bytes));
-    final result =
-        await ImageGallerySaver.saveImage(Uint8List.fromList(response.data));
-    print(result);
-    Navigator.pop(context);
+  Widget buttonDownload(context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      alignment: Alignment.bottomCenter,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              _save();
+            },
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Color(0xff1C1B1B).withOpacity(0.8),
+                  ),
+                  width: MediaQuery.of(context).size.width / 2,
+                ),
+                Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width / 2,
+                  padding:
+                  EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                  decoration: BoxDecoration(
+                      border:
+                      Border.all(color: Colors.white54, width: 1),
+                      borderRadius: BorderRadius.circular(30),
+                      gradient: LinearGradient(colors: [
+                        Color(0x36FFFFFF),
+                        Color(0x0EFFFFFF)
+                      ])),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "Set Wallpaper",
+                        style: TextStyle(
+                            fontSize: 16, color: Colors.white70),
+                      ),
+                      Text(
+                        "image will be saved in gallery",
+                        style: TextStyle(
+                            fontSize: 10, color: Colors.white70),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
+    );
   }
-
-  _askPermission() async {
-    if (Platform.isIOS) {
-      Map<PermissionGroup, PermissionStatus> permissions =
-          await PermissionHandler()
-              .requestPermissions([PermissionGroup.photos]);
-    } else {
-      PermissionStatus permission = await PermissionHandler()
-          .checkPermissionStatus(PermissionGroup.storage);
-    }
-  }
-}
 
 Widget _buildChip(String label) {
   return Chip(
@@ -201,17 +130,17 @@ Widget _buildChip(String label) {
   );
 }
 
-void _detailsModal(context) {
+Widget detailsModal(context) {
   final int likeCount = 999;
   final GlobalKey<LikeButtonState> _globalKey = GlobalKey<LikeButtonState>();
   final double buttonSize = 40.0;
-  showModalBottomSheet(
-      context: context,
-      builder: (BuildContext bc) {
         return Container(
-          height: MediaQuery.of(context).size.height * .30,
+          height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
-            color: Colors.white,
+              color: Color(0xffd2cccc).withOpacity(1),
+              gradient: LinearGradient(colors: [
+                  Color(0x36FFFFFF),
+              Color(0xE111111) ],),
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(50),
               topRight: Radius.circular(50),
@@ -293,7 +222,31 @@ void _detailsModal(context) {
             ),
           ),
         );
-      });
+}
+
+
+  _save() async {
+    if (Platform.isAndroid) {
+      await _askPermission();
+    }
+    var response = await Dio()
+        .get(widget.imgUrl, options: Options(responseType: ResponseType.bytes));
+    final result =
+    await ImageGallerySaver.saveImage(Uint8List.fromList(response.data));
+    print(result);
+    Navigator.pop(context);
+  }
+
+  _askPermission() async {
+    if (Platform.isIOS) {
+      Map<PermissionGroup, PermissionStatus> permissions =
+      await PermissionHandler()
+          .requestPermissions([PermissionGroup.photos]);
+    } else {
+      PermissionStatus permission = await PermissionHandler()
+          .checkPermissionStatus(PermissionGroup.storage);
+    }
+  }
 }
 
 class ReportTile extends StatelessWidget {
